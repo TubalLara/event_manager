9148c6df4356f92c564de7eb676b1ad6f3b5f681
Tubal Lara <tublargar@gmail.com>
2015-08-20 17:29:11 +0200

    email-fixed

--- eventManager/config/environments/development.rb
+++ eventManager/config/environments/development.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => 'larp.event.manager@gmail.com',
-    :password => 'uwtrgvyedxshixak',
+    :user_name => ENV['gmail_username'],
+    :password => ENV['gmail_password'],
     :authentication => "plain",
     :openssl_verify_mode  => 'none',
     :enable_starttls_auto => true
