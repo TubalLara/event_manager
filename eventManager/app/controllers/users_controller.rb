02d0c63938075edc1b6bf0ab4953c50f9d0e8049
Tubal Lara <tublargar@gmail.com>
2015-08-21 17:21:06 +0200

    adding images to users

--- eventManager/app/controllers/users_controller.rb
+++ eventManager/app/controllers/users_controller.rb
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
   private
 	  def user_params
 	    params.require(:user).permit(
-	      :name, :email, :password, :password_confirmation)
+	      :name, :email, :password, :password_confirmation, :avatar)
 	  end
 
 end
--- eventManager/db/schema.rb
+++ eventManager/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821085754) do
+ActiveRecord::Schema.define(version: 20150821145306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,10 +48,14 @@ ActiveRecord::Schema.define(version: 20150821085754) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "password_digest"
     t.string   "role"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
